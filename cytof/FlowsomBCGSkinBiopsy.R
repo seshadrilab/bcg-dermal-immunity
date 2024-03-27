eacaeb841e3e92a662974b6c3f2d0add577c08ff
Emma Bishop <bishemma1@gmail.com>
2024-03-26 17:12:06 -0700

    Add more comments, remove unused code

--- cytof/FlowsomBCGSkinBiopsy.R
+++ cytof/FlowsomBCGSkinBiopsy.R
@@ -1,4 +1,12 @@
 
+# Script for analyzing CyTOF data
+# Author: Mohau S Makatsa
+# Modified by: Emma Bishop
+
+#############################
+## Load libraries and data ##
+#############################
+
 library(readxl)
 library(CATALYST)
 library(flowCore)
@@ -32,22 +40,24 @@ md$timepoint <- factor(md$timepoint, levels = c("Day_3", "Day_15"))
 md$sample_id <- factor(md$sample_id, levels = md$sample_id[order(md$timepoint)])
 
 # Create SingleCellExperiment object
-## By default, this arcsinh transformers marker expresssions with a cofactor of 5.
+# By default, this arcsinh transformers marker expresssions with a cofactor of 5.
 sce <- prepData(fs, panel, md, features = panel$fcs_colname,
                 panel_cols = list(channel = "fcs_colname", antigen = "antigen"),
                 md_cols = list(file = "fcs_name", id = "sample_id", 
                                factors = c("patient_id", "timepoint", "group", "age", "sex")))
 
-## Diagnostic plots
-
-p <- plotExprs(sce, color_by = "timepoint")
-p$facet$params$ncol <- 10
-p
-
+# Number of cells per sample
 ncells <- n_cells(sce)
 ncells
 
-# write.csv(ncells, "C:/Users/Steven Makatsa/OneDrive - UW/Shared Documents - SeshadriLab/Members/YuKrystle/Projects_and_Data/BCG_Skin_Biopsies/Mass Cytometry/ProcessedData/Rerun FlowSOM_SM/out/BCG Skin Biopsy total CD45 count.csv", row.names=FALSE)
+######################
+## Diagnostic plots ##
+######################
+
+# Marker expression distribution
+p <- plotExprs(sce, color_by = "timepoint")
+p$facet$params$ncol <- 10
+p
 
 # Bar plot of total CD45+ count per sample
 cellcount_bar <- plotCounts(sce, group_by = "sample_id", color_by = "timepoint") +
@@ -75,7 +85,6 @@ cairo_pdf(file = file.path(outdir, "plots/SuppFig3_cellcount_bar.pdf"),
           width = 5, height = 3, bg = "transparent", family = "Arial")
 print(cellcount_bar)
 dev.off()
-
 
 # Non-redundancy scores (NRS) across samples for all markers
 nrs_boxplot <- plotNRS(sce, color_by = "timepoint") +
@@ -107,8 +116,7 @@ dev.off()
 # MDS plot
 pbMDS(sce, color_by = "timepoint", label_by = "sample_id")
 
-## Perform unsupervised clustering
-
+# Perform unsupervised clustering
 set.seed(1234)
 sce <- cluster(sce, features = "type",
                xdim = 12, ydim = 12, maxK = 24, seed = 1234)
@@ -118,14 +126,18 @@ plotExprHeatmap(sce, features = "type",
                       by = "cluster_id", k = "meta24", 
                       bars = TRUE, perc = TRUE)
 
-## Visual representation with UMAP
+#########################
+## UMAP visualizations ##
+#########################
 
-# run UMAP
+# Run dimension reduction
 set.seed(1234)
 sce <- runDR(sce, "UMAP", cells = 10000, features = "type")
 
+# UMAP visualization of CD3 expression
 plotDR(sce, "UMAP", color_by = "CD3")
 
+# UMAPs visualization of expression of multiple markers
 p1 <- plotDR(sce, "UMAP", color_by = "CD68") + 
   theme(legend.position = "none")
 p2 <- plotDR(sce, "UMAP", color_by = "CD163") + 
@@ -149,34 +161,39 @@ p10 <- plotDR(sce, "UMAP", color_by = "CD66abce") +
 
 plot_grid(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, nrow = 2)
 
-# facet by sample
+# UMAP showing un-annotated clusters
 plotDR(sce, "UMAP", color_by = "meta24")
 
-# facet by timepoint 
+# UMAP faceted by timepoint and sample
 plotDR(sce, "UMAP", color_by = "meta24", facet_by = "timepoint")
-
 plotDR(sce, "UMAP", color_by = "meta24", facet_by = "sample_id")
 
 ## Apply manual merging
-
-merging_table1 <- here::here("cytof/data/BCG_challenge_cluster_annotations_24.xlsx")
-merging_table1 <- read_excel(merging_table1)
 head(data.frame(merging_table1))
 
-# convert to factor with merged clusters
+# Convert to factor with merged clusters
 merging_table1$new_cluster <- factor(merging_table1$new_cluster)
 
-# apply manual merging
+# Apply manual merging
 sce <- mergeClusters(sce, k = "meta24", 
                      table = merging_table1, id = "Annotations", overwrite = TRUE)
 
+# UMAP visualization of manual annotations
 plotDR(sce, "UMAP", color_by = "Annotations")
+
+# Faceted by timepoint and sample
+plotDR(sce, "UMAP", color_by = "Annotations", facet_by = "timepoint")
+plotDR(sce, "UMAP", color_by = "Annotations", facet_by = "sample_id")
+
+########################
+## Annotated heatmaps ##
+########################
 
 # Heatmap labelled by cluster and our annotations
 plotExprHeatmap(sce, features = "type", 
                 by = "cluster_id", k = "meta24", m = "Annotations")
 
-# Heatmap labelled by annotation with dendrogram
+# Heatmap labelled by our annotations with dendrogram
 plotExprHeatmap(sce, features = "type",
                 by = "cluster_id", k = "Annotations", 
                 bars = TRUE, perc = TRUE)
@@ -189,36 +206,31 @@ cytof_heat_plot <- plotExprHeatmap(sce, features = "type",
                 col_dend = FALSE)
 cytof_heat_plot
 
+# Save
 cairo_pdf(file = file.path(outdir, "plots/Fig4_cytof_heatmap.pdf"), 
           width=9, height=5,
           onefile = TRUE, bg = "transparent", family = "Arial")
 print(cytof_heat_plot)
 dev.off()
 
-
-plotDR(sce, "UMAP", color_by = "Annotations", facet_by = "timepoint")
-
-plotDR(sce, "UMAP", color_by = "Annotations", facet_by = "sample_id")
-
-# remove a BCG07_02 sample
-filterSCE(sce, sample_id != "BCG07_2")
+#####################################
+## Differential abundance analysis ##
+#####################################
 
 FDR_cutoff <- 0.05
 
-## Differential analysis
-
+# Boxplots of cell type proportions
 plotAbundances(sce, k = "Annotations", by = "cluster_id", 
                       shape_by = "group", group_by = "timepoint")
 
+# Calculate proportions with diffcyt
 ei <- metadata(sce)$experiment_info
-(da_formula1 <- createFormula(ei, 
+da_formula1 <- createFormula(ei, 
                               cols_fixed = "timepoint", 
-                              cols_random = "sample_id"))
-
-(da_formula2 <- createFormula(ei, 
+                              cols_random = "sample_id")
+da_formula2 <- createFormula(ei, 
                               cols_fixed = "timepoint", 
-                              cols_random = c("sample_id", "patient_id")))
-
+                              cols_random = c("sample_id", "patient_id"))
 contrast <- createContrast(c(1, 1))
 
 da_res1 <- diffcyt(sce, 
@@ -230,19 +242,14 @@ da_res2 <- diffcyt(sce,
                    analysis_type = "DA", method_DA = "diffcyt-DA-GLMM",
                    clustering_to_use = "Annotations", verbose = FALSE)
 
-names(da_res1)
+# Take a detailed look
+# names(da_res1)
+# rowData(da_res1$res) 
+# table(rowData(da_res1$res)$p_adj < FDR_cutoff)
+# table(rowData(da_res2$res)$p_adj < FDR_cutoff)
 
-rowData(da_res1$res) 
-
-table(rowData(da_res1$res)$p_adj < FDR_cutoff)
-
-table(rowData(da_res2$res)$p_adj < FDR_cutoff)
-
-topTable(da_res2, show_props = TRUE, format_vals = TRUE, digits = 2)
-
-table <- topTable(da_res2, show_props = TRUE, format_vals = TRUE, digits = 2)
-
-table <- as.data.frame(table)
+table <- topTable(da_res2, show_props = TRUE, format_vals = TRUE, digits = 2) %>%
+  as.data.frame()
 
 # Reformat for use by other scripts
 table_out <- table %>%
@@ -260,6 +267,3 @@ table_out <- table %>%
          .after = "cluster_id")
 
 write_csv(table_out, file.path(outdir, "processed_data/BCG_Skin_Biopsy_CD45_Subsets.csv"))
-
-plotDiffHeatmap(sce, rowData(da_res2$res), all = TRUE, fdr = FDR_cutoff)
-
